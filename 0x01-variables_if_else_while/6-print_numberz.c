07709655107b95dd3a0a9d97adb18cce804c3a45
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 11:43:39 -0700

    Hello World

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - entry position
+*Return: end the program
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar("%d", i);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
