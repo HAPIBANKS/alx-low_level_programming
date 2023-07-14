caaf529055814f4f8588285fc6fd0a0858f7d3b4
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 12:46:00 -0700

    Hello World

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - entry position
+*Return: end the program
+*/
+int main(void)
+{
+	char ch = '0';
+	char next_ch;
+
+	while (ch <= '9')
+	{
+	putchar(ch);
+	next_ch = ch + 1;
+	putchar(next_ch);
+	ch = next_ch;
+	}
+
+	ch = 'a';
+	while (ch <= 'f')
+	{
+	putchar(ch);
+	next_ch = ch + 1;
+	putchar(next_ch);
+	ch = next_ch;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
