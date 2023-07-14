7c644553cf7823ad1d89d894d25a5801fb556387
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 09:44:09 -0700

    Hello World

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
-	printf("Last digits")
+	printf("Last digits");
 	printf("of %d is %d and less than 6 and not 0\n", n, last_digit);
 	}
 
