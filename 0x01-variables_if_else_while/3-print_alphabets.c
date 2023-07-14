9851a081c04440ba37e4ac6bbde0bd02a4f92639
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 10:25:21 -0700

    Hello World

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - entry position
+*Return: end the program
+*/
+int main(void)
+{
+	char ch = 'a'; 'A'
+
+	while (ch <= 'z'; 'Z')
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
