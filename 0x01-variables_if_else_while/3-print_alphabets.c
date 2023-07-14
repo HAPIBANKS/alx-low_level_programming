cac811890c665b472139e8f73e68fad2fba18bc7
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 10:30:21 -0700

    Hello World

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,19 @@
 */
 int main(void)
 {
-	char ch = 'a'; 'A'
+	char ch = 'a';
 
-	for (ch <= 'z'; 'Z')
+	while (ch <= 'z';)
 	{
 	putchar(ch);
 	ch++;
 	}
-
+	upper_ch = ch - 'a' + 'A';
+	while (upper_ch <= 'Z')
+	{
+	putchar(upper_ch);
+	upper_ch++;
+	}
 	putchar('\n');
 
 	return (0);
