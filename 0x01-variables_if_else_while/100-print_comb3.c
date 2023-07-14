be43d12ce3454104f7d83c56da5593805253daf8
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 13:42:13 -0700

    Hello World

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,17 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = 0; j <= 9; j++)
+	for (j = 1; j <= 9; j++)
 	{
 	if (i < j)
 	{
 	putchar(i + '0');
 	putchar(j + '0');
+	if (i != 8 || j != 9)
+	{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	}
 	}
