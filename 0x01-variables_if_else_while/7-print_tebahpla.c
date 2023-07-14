d349df5663e14e08a232425cc74e23039bae6c5f
Adebisi Gbenga <posiayo1@gmail.com>
2023-07-14 12:25:46 -0700

    Hello World

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - entry position
+*Return: end the program
+*/
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch <= 'a`')
+	{
+	putchar(ch);
+	ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
