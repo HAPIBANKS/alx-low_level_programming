99833305dfa86f8e3227df47b1c06efd20630810
HAPIBANKS <posiayo1@gmail.com>
2023-07-22 03:52:27 -0700

    low level language

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+
+}
