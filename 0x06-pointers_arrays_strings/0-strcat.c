2769c83c43d745d2c84bf416efee8b7a33bea298
HAPIBANKS <posiayo1@gmail.com>
2023-07-26 08:16:07 -0700

    cisfun

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_strcat - main
+*@dest: string 1
+*@src: string 2
+*Return: show result
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+	dest[i + j] = src[j];
+	}
+	return (dest);
+}
