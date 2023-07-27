37aa0d310465be754f12e1430e7cd3e6ddebcc52
HAPIBANKS <posiayo1@gmail.com>
2023-07-27 03:47:20 -0700

    cisfuneh

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*_strcmp - main
+*@s1: data 1
+*@s2: data 2
+*Return: always 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] - s2[i] == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
