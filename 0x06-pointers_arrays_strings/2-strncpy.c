bd3724511fec7d82a48a7d3ebd8b45c15061bd24
HAPIBANKS <posiayo1@gmail.com>
2023-07-27 10:28:35 -0700

    cisfun

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*_strncpy - main
+*@dest: destination
+*@src: initial string
+*@n: number
+*Return: always 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
