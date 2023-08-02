7c72a100791e7a850f66c6326a11edde8382a440
HAPIBANKS <posiayo1@gmail.com>
2023-08-02 10:09:09 -0700

    recursion_in_reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_print_rev_recursion - repetition
+*@s: entry string
+*Return: return void
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
