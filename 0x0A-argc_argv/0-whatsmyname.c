e71b1a01bb656aab9ea9ed924262000505c70b6f
HAPIBANKS <posiayo1@gmail.com>
2023-08-09 07:13:25 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - argv, argc
+*Return: always 0
+*@argc: number of argument
+*@argv: argument variable
+*/
+
+int main(int argc, char argv)
+{
+	char *program_name = __FILE__;
+
+	printf("%s\n", program_name);
+	return (0);
+}
