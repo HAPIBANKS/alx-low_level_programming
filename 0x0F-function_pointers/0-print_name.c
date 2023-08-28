95e66d3f6de2cc374a28de62a07d775260f81474
HAPIBANKS <posiayo1@gmail.com>
2023-08-28 10:19:58 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,13 @@
  * print_name - print name
  * @name: name of a person
  * @f: function pointer
- * Return: nothing
+ * Return: always nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-		return;
-	f(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
