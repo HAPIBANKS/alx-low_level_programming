f565f633f2e9b101b4bd2974fda9401bad7e6a8f
HAPIBANKS <posiayo1@gmail.com>
2023-08-28 14:48:36 -0700

    sum of elements in a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**sum_listint - sum lists
+*@head: head of nodes
+*Return: return int
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
