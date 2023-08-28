a874f1c02ed755c20f3af607557cd42f5df47398
HAPIBANKS <posiayo1@gmail.com>
2023-08-28 10:14:16 -0700

    print all the element in a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*print_listint - print all element
+*@h: head of the node
+*Return: return size_t
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	nodes++;
+	h = h->next;
+	}
+	return (nodes);
+}
