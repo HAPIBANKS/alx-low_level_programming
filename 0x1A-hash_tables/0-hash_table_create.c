c10c3ce9e25d1dadc63da013921b157943517207
HAPIBANKS <posiayo1@gmail.com>
2023-11-30 09:48:09 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - a function that creates hash table
+ *@size: size of the table (array)
+ *
+ *Return: it returns hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size);
+{
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+
+	if (!hash_table)
+	{
+		return (NULL);
+	}
+
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(unsigned long int));
+	return (hash_table);
+}
