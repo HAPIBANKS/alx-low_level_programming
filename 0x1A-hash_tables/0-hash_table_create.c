7101abf0e0414a888de39da5ceab454674f11aca
HAPIBANKS <posiayo1@gmail.com>
2023-11-30 10:06:08 -0800

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  *
  *Return: it returns hash table
  */
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
