8c5d466cbbfea61dca4fbee91ea4a4d0ed44bb7e
HAPIBANKS <posiayo1@gmail.com>
2023-12-02 20:18:36 -0800

    key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ *key_index - a functionn that gives the index of a key
+ *@key: key to be taken
+ *@size: size of the array
+ *Return:an index(address of the key in the array)
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
+	return (index);
+}
