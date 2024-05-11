f388db66af958b1d51ba0899522f1d1bf38c4624
HAPIBANKS <posiayo1@gmail.com>
2024-05-11 15:02:40 +0100

    linear search algo

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,14 +12,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (value)
+	if (array == NULL || size == 0)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			printf("value checked array[%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-		}
+		printf("value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
